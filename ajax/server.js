8db4567667366b81e338696ecf2a5ea26997f940
otavioHirooka <otaviolbh@hotmail.com>
2020-05-07 15:42:20 -0300

    axios, fetch e jquery

--- ajax/server.js
+++ ajax/server.js
@@ -27,5 +27,22 @@ app.post('/upload', (req, res) => {
     })
 })
 
-// app.get('/teste', (req, res) => res.send('Ok'))
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 app.listen(8080, () => console.log('Executando.....'))
